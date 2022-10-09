120143db9c71920db9a9d11cf8898785bc697581
LikithTs <likith.ts@gmail.com>
2022-10-09 09:38:07 +0530

    Added support for localization Added new card for home view

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      // child: SplashScreenPage(),
-      child: AddCarCard(),
+      child: SplashScreenPage(),
     );
   }
 }
