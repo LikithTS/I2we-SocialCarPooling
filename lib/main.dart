5e7413ec6221d6674412fbd2994f59c284613d22
ramprasadreddy <gajjalaramprasadreddy@gmail.com>
2022-10-10 17:08:05 +0530

    dashboard tab layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:socialcarpooling/util/configuration.dart';
-import 'package:socialcarpooling/utils/Localization.dart';
-import 'package:socialcarpooling/view/home/home_cards/add_car_home_view.dart';
 import 'package:socialcarpooling/util/dimens.dart';
+import 'package:socialcarpooling/utils/Localization.dart';
 import 'package:socialcarpooling/view/splash/splash_page.dart';
 
 void main() {
@@ -16,6 +14,10 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     SystemChrome.setSystemUIOverlayStyle(
         SystemUiOverlayStyle(statusBarColor: Colors.blue));
     return ScreenUtilInit(
@@ -27,11 +29,12 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           locale: const Locale('en'),
           localizationsDelegates: const [DemoLocalizationsDelegate()],
-          supportedLocales: const [Locale('en','')],
+          supportedLocales: const [Locale('en', '')],
           theme: ThemeData(
             textTheme: GoogleFonts.poppinsTextTheme(
               Theme.of(context).textTheme,
-            ),),
+            ),
+          ),
           home: child,
         );
       },
---
+++ lib/view/home/tab_utils/home_icon_text_form.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeTextIconForm extends StatelessWidget {
+  final String hint;
+  final IconData prefixIcon;
+
+  const HomeTextIconForm(
+      {Key? key, required this.hint, required this.prefixIcon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: new BorderRadius.circular(5.0),
+          boxShadow: [
+            BoxShadow(color: Colors.grey, blurRadius: 2.0, spreadRadius: 0.4)
+          ]),
+      child: TextFormField(
+        textAlign: TextAlign.start,
+        decoration: InputDecoration(
+            fillColor: Colors.grey,
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            hintText: hint,
+            prefixIcon: Icon(
+              prefixIcon,
+              color: Colors.grey,
+            )),
+      ),
+    );
+  }
+}
