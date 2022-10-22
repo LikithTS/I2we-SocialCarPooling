0faa729806ecc071bab0bcd0dd39cf057eaa3174
ramprasadreddy <gajjalaramprasadreddy@gmail.com>
2022-10-22 09:46:34 +0530

    my cars ui fix

--- lib/utils/widget_functions.dart
+++ lib/utils/widget_functions.dart
@@ -55,7 +55,6 @@ Widget smallLightText(String text, Alignment alignement,
 Widget headerText(String title) => Container(
     margin: EdgeInsets.symmetric(horizontal: 20.w),
     child: Align(
-      alignment: Alignment.topLeft,
       child: Text(title,
           style: TextStyle(
               fontSize: 22.sp,
@@ -65,10 +64,37 @@ Widget headerText(String title) => Container(
               fontFamily: 'Poppins')),
     ));
 
-Widget mycarTextWidget(String? title, Color color, double size) => Text(title!,
-    style: TextStyle(
-        fontSize: size,
-        decoration: TextDecoration.none,
-        color: color,
-        fontWeight: FontWeight.bold,
-        fontFamily: 'Poppins'));
+Widget mycarTextWidget(String? title, Color color, double size) => Container(
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Text(title!,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                fontSize: size,
+                decoration: TextDecoration.none,
+                color: color,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins')),
+      ),
+    );
+
+Widget smallTextWithNoMargin(String text, Alignment alignement,
+        [TextAlign? textAlign]) =>
+    Container(
+        child: Align(
+      alignment: alignement,
+      child: Expanded(
+        child: Text(
+          text,
+          textAlign: textAlign ?? TextAlign.start,
+          style: TextStyle(
+              fontSize: 14.sp,
+              height: 1.3,
+              color: Colors.black,
+              fontWeight: FontWeight.normal,
+              decoration: TextDecoration.none,
+              fontFamily: 'Poppins'),
+          maxLines: 3,
+        ),
+      ),
+    ));
--- lib/view/myvehicle/my_car_screen.dart
+++ lib/view/myvehicle/my_car_screen.dart
@@ -11,7 +11,6 @@ import '../../utils/widget_functions.dart';
 import '../../widgets/text_widgets.dart';
 
 class MyCarsScreen extends StatelessWidget {
-
   final List<CarDetailsResponse> carList;
 
   const MyCarsScreen(this.carList, {Key? key}) : super(key: key);
@@ -20,175 +19,188 @@ class MyCarsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: SafeArea(
-          child: Scaffold(
-            body: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.arrow_back),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      headerText(
-                          DemoLocalizations.of(context)?.getText("my_cars_title") ?? "")
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: carList.length,
-                    itemBuilder: (context, index) {
-                      return Dismissible(
-                        key: Key(carList[index].toString()),
-                        direction: DismissDirection.endToStart,
-                        background: slideLeftBackground(),
-                        onDismissed: (DismissDirection direction) {
-                          if (direction == DismissDirection.endToStart) {
-                            print('Remove item');
-                          }
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                          height: 150,
-                          width: double.maxFinite,
-                          child: Card(
-                            elevation: 5,
-                            child: Row(
-                              children: [
-                                const Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: CircleAvatar(
-                                    radius: 30, // Image radius
-                                    backgroundImage: NetworkImage(
-                                        'http://m.gettywallpapers.com/wp-content/uploads/2021/09/Hulk-Wallpaper.jpg'),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                    MainAxisAlignment.start,
-                                    children: [
-                                      mycarTextWidget(
-                                          carList[index].carType, primaryColor, 18.sp),
-                                      Padding(
-                                        padding: EdgeInsets.all(8.0),
-                                        child: mycarTextWidget(
-                                            carList[index].carName, Colors.black, 12.sp),
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsets.all(8.0),
-                                        child: mycarTextWidget(
-                                            carList[index].regNumber, primaryColor, 13.sp),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Column(
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Container(
-                                              padding: const EdgeInsets.fromLTRB(
-                                                  0, 0, 0, 0),
-                                              height: 50,
-                                              width: 170,
-                                              child: Card(
-                                                elevation: 5,
-                                                child: Row(
-                                                  mainAxisAlignment:
-                                                  MainAxisAlignment.spaceEvenly,
-                                                  children: [
-                                                    Column(
-                                                      mainAxisAlignment:
-                                                      MainAxisAlignment.center,
-                                                      children: [
-                                                        Icon(Icons.drive_eta),
-                                                        smallLightText("seats",
-                                                            Alignment.center),
-                                                      ],
-                                                    ),
-                                                    const RotatedBox(
-                                                      quarterTurns: 1,
-                                                      child: Divider(),
-                                                    ),
-                                                    Column(
-                                                      mainAxisAlignment:
-                                                      MainAxisAlignment.center,
-                                                      children: [
-                                                        primaryThemeTextNormal(
-                                                            context, carList[index].seatingCapacity.toString()),
-                                                        smallLightText("available",
-                                                            Alignment.center),
-                                                      ],
-                                                    ),
-                                                    const RotatedBox(
-                                                      quarterTurns: 1,
-                                                      child: Divider(),
-                                                    ),
-                                                    Column(
-                                                      mainAxisAlignment:
-                                                      MainAxisAlignment.center,
-                                                      children: [
-                                                        primaryThemeTextNormal(
-                                                            context, carList[index].offeringSeat.toString()),
-                                                        smallLightText("offered",
-                                                            Alignment.center)
-                                                      ],
-                                                    ),
-                                                  ],
-                                                ),
-                                              ),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.start,
-                                        children: [
-                                          smallText(
-                                              "set default :", Alignment.topLeft),
-                                          Container(
-                                            width: 40,
-                                            height: 30,
-                                            child: FittedBox(
-                                              fit: BoxFit.contain,
-                                              child: CupertinoSwitch(
-                                                value: carList[index].drivingStatus ?? false,
-                                                onChanged: (value) {
-                                                  log("Default value changed $value");
-                                                },
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
+      child: Scaffold(
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
                     },
                   ),
-                ),
-              ],
+                  headerText(
+                      DemoLocalizations.of(context)?.getText("my_cars_title") ??
+                          "")
+                ],
+              ),
             ),
-          ),
-        ));
+            Expanded(
+              child: ListView.builder(
+                itemCount: carList.length,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    key: Key(carList[index].toString()),
+                    direction: DismissDirection.endToStart,
+                    background: slideLeftBackground(),
+                    onDismissed: (DismissDirection direction) {
+                      if (direction == DismissDirection.endToStart) {
+                        print('Remove item');
+                      }
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      height: 150,
+                      width: double.maxFinite,
+                      child: Card(
+                        elevation: 5,
+                        child: Row(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: CircleAvatar(
+                                radius: 30, // Image radius
+                                backgroundImage: NetworkImage(
+                                    'http://m.gettywallpapers.com/wp-content/uploads/2021/09/Hulk-Wallpaper.jpg'),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  mycarTextWidget(carList[index].carType,
+                                      primaryColor, 18.sp),
+                                  mycarTextWidget(carList[index].carName,
+                                      Colors.black, 12.sp),
+                                  mycarTextWidget(carList[index].regNumber,
+                                      primaryColor, 13.sp)
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Container(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              0, 0, 0, 0),
+                                          height: 50,
+                                          width: 170,
+                                          child: Card(
+                                            elevation: 5,
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  children: [
+                                                    const Icon(
+                                                      Icons
+                                                          .airline_seat_recline_normal,
+                                                      color: primaryColor,
+                                                    ),
+                                                    smallLightText("seats",
+                                                        Alignment.center),
+                                                  ],
+                                                ),
+                                                const RotatedBox(
+                                                  quarterTurns: 1,
+                                                  child: Divider(),
+                                                ),
+                                                Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  children: [
+                                                    primaryThemeTextNormal(
+                                                        context,
+                                                        carList[index]
+                                                            .seatingCapacity
+                                                            .toString()),
+                                                    smallLightText("available",
+                                                        Alignment.center),
+                                                  ],
+                                                ),
+                                                const RotatedBox(
+                                                  quarterTurns: 1,
+                                                  child: Divider(),
+                                                ),
+                                                Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  children: [
+                                                    primaryThemeTextNormal(
+                                                        context,
+                                                        carList[index]
+                                                            .offeringSeat
+                                                            .toString()),
+                                                    smallLightText("offered",
+                                                        Alignment.center)
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        20.0, 8.0, 8.0, 8.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        smallTextWithNoMargin(
+                                            "set default :", Alignment.topLeft),
+                                        Container(
+                                          width: 40,
+                                          height: 30,
+                                          child: FittedBox(
+                                            fit: BoxFit.contain,
+                                            child: CupertinoSwitch(
+                                              value: carList[index]
+                                                      .drivingStatus ??
+                                                  false,
+                                              onChanged: (value) {
+                                                log("Default value changed $value");
+                                              },
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 
   void delete() {}
